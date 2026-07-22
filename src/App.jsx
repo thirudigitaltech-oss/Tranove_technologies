dca77595ea342d72453fe4179f360b2f8ff95adc
thirudigitaltech-oss <thirudigitaltech@gmail.com>
2026-07-22 17:51:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -261,7 +261,7 @@ function Header({ menuOpen, setMenuOpen }) {
     <header className="dcs-header">
       <div className="dcs-container dcs-header-row">
         <a href="#top" className="dcs-logo">
-          <span className="dcs-logo-mark"><Image source={Godist} style={{ width: 200, height: 200 }} /></span>
+          <span className="dcs-logo-mark"><Img src={Godist} style={{ width: 200, height: 200 }} /></span>
          
         </a>
 
